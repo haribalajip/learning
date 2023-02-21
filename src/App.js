e03030c67c0348fd92596b0f6b19448b51d657aa
Hariharasuthan B <hari111.balaji@gmail.com>
2023-02-21 09:01:08 +0530

    chore: error page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Root from "./pages/Root";
+import Errorpage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <Errorpage />,
     children: [
       { path: "/about-me", element: <AboutMe /> },
       { path: "/home", element: <Home /> },
