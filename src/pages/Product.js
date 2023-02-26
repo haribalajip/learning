1b1bb233e4c0a9a8cec38f915163ab5067c05528
Hariharasuthan B <hari111.balaji@gmail.com>
2023-02-26 20:01:38 +0530

    comment

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,6 +3,6 @@ import { useLoaderData, useParams } from "react-router-dom";
 export default function Product() {
   let params = useParams();
   let loaderData = useLoaderData();
-  console.log(loaderData);
+  console.log(loaderData); // Logs true
   return <h4>A sample Product with id {params.id}</h4>;
 }
