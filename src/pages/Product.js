75b8604037aa1f19d247658f05400450f97e47ea
Hariharasuthan B <hari111.balaji@gmail.com>
2023-02-26 20:01:26 +0530

    chore: loader data logged

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,8 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export default function Product() {
   let params = useParams();
+  let loaderData = useLoaderData();
+  console.log(loaderData);
   return <h4>A sample Product with id {params.id}</h4>;
 }
